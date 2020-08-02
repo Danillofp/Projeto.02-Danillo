d648d64219956deee1ef6ab4228077371ec2d5ef
Luka <luanafernandes73@gmail.com>
2020-08-02 16:31:20 -0400

    Update Aluno

--- Projeto.Escola/Entity/Aluno.cs
+++ Projeto.Escola/Entity/Aluno.cs
@@ -14,11 +14,23 @@ namespace Projeto.Escola.Entity
         private Turma turma;
         private string matricula;
 
+        public Aluno()
+        {
+
+        }
+
+        public Aluno(string nome, int idade, Turma turma, string matricula)
+        {
+            Nome = nome;
+            Idade = idade;
+            Turma = turma;
+            Matricula = matricula;
+        }
+
+        public int IdAluno { get => idAluno; set => idAluno = value; }
         public string Nome { get => nome; set => nome = value; }
-        
         public int Idade { get => idade; set => idade = value; }
         public Turma Turma { get => turma; set => turma = value; }
         public string Matricula { get => matricula; set => matricula = value; }
-        public int IdAluno { get => idAluno; set => idAluno = value; }
     }
 }
